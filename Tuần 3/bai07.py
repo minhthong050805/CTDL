6b8a4cfc7c8afd475931e7b01fd7036abea12998
minhthong050805 <minhthong050805@github.com>
2026-06-10 00:33:54 +0700

    upload Tuan2

--- Tuần 3/bai07.py
+++
@@ -1,9 +0,0 @@
-a = ['d', 'a', 'c', 'b']  # //vidu
-
-n = len(a)
-for i in range(n - 1):
-    for j in range(n - 1 - i):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
-
-print(a)  # ['a', 'b', 'c', 'd']
